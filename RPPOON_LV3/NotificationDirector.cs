e5b6fcf77f5febed34ba4544d840878361775928
Valentin Loboda <vloboda@etfos.hr>
2020-04-24 18:01:37 +0200

    6.zad

---
+++ RPPOON_LV3/NotificationDirector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPPOON_LV3
+{
+    class NotificationDirector
+    {
+
+        public ConsoleNotification AlertNotification(string author)
+        {
+            return new ConsoleNotification(author, "Naslov", "Defaultni tekst obavijesti", DateTime.Now, Category.ALERT, ConsoleColor.Cyan);
+          
+        }
+
+        public ConsoleNotification InfoNotification(string author)
+        {
+            return new ConsoleNotification(author, "Naslov", "Defaultni tekst obavijesti", DateTime.Now, Category.INFO, ConsoleColor.Cyan);
+
+        }
+
+        public ConsoleNotification ErrorNotification(string author)
+        {
+            return new ConsoleNotification(author, "Naslov", "Defaultni tekst obavijesti", DateTime.Now, Category.ERROR, ConsoleColor.Cyan);
+
+        }
+
+    }
+}
