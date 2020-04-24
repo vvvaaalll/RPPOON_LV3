1f219b187041e05b7e292b795cc6d359c8b279b8
Valentin Loboda <vloboda@etfos.hr>
2020-04-24 17:50:08 +0200

    5.zad

---
+++ RPPOON_LV3/IBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPPOON_LV3
+{
+    interface IBuilder
+    {
+        ConsoleNotification Build();
+        IBuilder SetAuthor(String author);
+        IBuilder SetTitle(String title);
+        IBuilder SetTime(DateTime time);
+        IBuilder SetLevel(Category level);
+        IBuilder SetColor(ConsoleColor color);
+        IBuilder SetText(String text);
+
+    }
+}
---
+++ RPPOON_LV3/NotificationBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPPOON_LV3
+{
+    class NotificationBuilder : IBuilder
+    {
+        public String author = "Valentin Loboda";
+        public String title = "naslov";
+        public String text = "ovo bi bila neka defaultna obavijest";
+        public DateTime timestamp = DateTime.Now;
+        public Category level = Category.INFO;
+        public ConsoleColor color = ConsoleColor.Green;
+
+      
+
+        public ConsoleNotification Build()
+        {
+            return new ConsoleNotification(author, title, text, timestamp, level, color); 
+        }
+
+        public IBuilder SetAuthor(string author)
+        {
+            this.author = author;
+            return this;
+        }
+
+        public IBuilder SetColor(ConsoleColor color)
+        {
+            this.color = color;
+            return this;
+        }
+
+        public IBuilder SetLevel(Category level)
+        {
+            this.level = level;
+            return this;
+        }
+
+        public IBuilder SetText(string text)
+        {
+            this.text = text;
+            return this;
+        }
+
+        public IBuilder SetTime(DateTime time)
+        {
+            this.timestamp = time;
+            return this;
+        }
+
+        public IBuilder SetTitle(string title)
+        {
+            this.title = title;
+            return this;
+        }
+    }
+}
